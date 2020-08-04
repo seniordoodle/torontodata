da19b42a911ba183dae958693ecf5eed0701a575
Michael MacDonald <michael.macd@hotmail.com>
2020-08-04 11:04:58 -0400

    Updates

---
+++ precache-manifest.ce08002575a0a73e6b56bf725fd365c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda85330535d2a131c1bef2eb91b7fe4",
+    "url": "/torontodata/index.html"
+  },
+  {
+    "revision": "c41ddb9811af02f6c737",
+    "url": "/torontodata/static/css/main.7a9c5c92.chunk.css"
+  },
+  {
+    "revision": "64530c0494950eac1db8",
+    "url": "/torontodata/static/js/2.5cb4fef0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/torontodata/static/js/2.5cb4fef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41ddb9811af02f6c737",
+    "url": "/torontodata/static/js/main.ad074046.chunk.js"
+  },
+  {
+    "revision": "bf83ed3b5e51becb3c8d",
+    "url": "/torontodata/static/js/runtime-main.55f1c3be.js"
+  }
+]);
