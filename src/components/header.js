fa06f1c86feeafbfbc752c7264996f7b542df3d3
Michael MacDonald <michael.macd@hotmail.com>
2020-10-06 17:14:23 -0400

    blah

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
 	return (
 		<div className="header_01">
-			<NavLink exact to=''> Home </NavLink> 
+			<NavLink exact to='/Home'> Home </NavLink> 
 			<NavLink exact to='/rbc'> RBC </NavLink> 
 			<NavLink exact to='/about'> about </NavLink> 
 			<NavLink exact to='/cbs'> CBS </NavLink> 
