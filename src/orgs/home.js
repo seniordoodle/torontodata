e9d8705a1c5c8a87943df5d481b6bb0c20c074df
Michael MacDonald <michael.macd@hotmail.com>
2020-10-06 17:18:48 -0400

    aa

--- src/orgs/home.js
+++ src/orgs/home.js
@@ -4,6 +4,7 @@ class Home extends Component {
 	render() {
 		return ( 
 			<div> 
+                <p>  HOME section </p>
 				<img className='MainImg' alt="" src={"https://raw.githubusercontent.com/seniordoodle/torontodata/master/src/Infograph/InfoKidney.png"} />
 			</div>
 		);
