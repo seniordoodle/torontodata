500e4c90be274c34b32b2b6f920ee8a771afece8
Michael MacDonald <michael.macd@hotmail.com>
2020-10-06 16:40:16 -0400

    Added infograph to main page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import ReactGA from 'react-ga';
 
+
 // components
 import Rbc from './orgs/rbc';
 import Cbs from './orgs/cbs';
+import Home from './orgs/home';
 import Header from './components/header';
 import Footer from './components/footer';
 import About from './components/about';
@@ -23,10 +25,12 @@ function App() {
 		<BrowserRouter>
 			< Header />
 			<div className='edgepadding'>
+			
 				<Switch>
 					<Route path='/rbc' component={ Rbc } />
 					<Route path='/about' component={ About } />
 					<Route path='/cbs' component={ Cbs } />
+					<Route exact path='/' component={ Home } />
 				</Switch>
 				< Footer />
 			</div>
