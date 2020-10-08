dbed50ca153618c1295c53811760409ecc99a978
Michael MacDonald <michael.macd@hotmail.com>
2020-10-08 12:44:15 -0400

    cbs testing

--- src/orgs/cbs.js
+++ src/orgs/cbs.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
 
 class Cbs extends Component { 
-
-	constructor() {
-		super();
-		this.state= {
-			count:0
-		}
-	}
-
-	incrementCount = () => {
-		this.setState({
-			count:this.state.count+1
-		})
-	}
-
-	decrementCount = () => {
-		this.setState({
-			count:this.state.count-1
-		})
-	}
-
-	render(test) 
-	{
-		let { count } = this.state;
+	render() {
 		return ( 
-			<div>
-				<div className="count">
-					<h3>Count:</h3>
-					<h1>{count}</h1>
-				</div>
-				<h2>Count: { count } </h2>
-				<button onClick = { () => this.incrementCount() }> </button>
-				<button onClick = { () => this.decrementCount() }> </button>
+			<div> 
+				<p> CBS section </p>
+				
 			</div>
-		)
+		);
 	}
 }
 
