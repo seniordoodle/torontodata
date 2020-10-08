c88571fcc6ceaea99a9e5b64c21138ce675c3466
Michael MacDonald <michael.macd@hotmail.com>
2020-10-08 12:44:15 -0400

    cbs testing

--- src/orgs/home.js
+++ src/orgs/home.js
@@ -5,7 +5,6 @@ class Home extends Component {
 		return ( 
 			<div> 
                 <p>  HOME section </p>
-				<img className='MainImg' alt="" src={"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F7%2F77%2FGoogle_Images_2015_logo.svg%2F1200px-Google_Images_2015_logo.svg.png&f=1&nofb=1"} />
 			</div>
 		);
 	}
