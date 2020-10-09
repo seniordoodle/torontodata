e690f08d4099561f43f996f258397e02992c8e60
Michael MacDonald <michael.macd@hotmail.com>
2020-10-08 23:47:30 -0400

    Update cbs.js

--- src/orgs/cbs.js
+++ src/orgs/cbs.js
@@ -4,7 +4,11 @@ class Cbs extends Component {
 	render() {
 		return ( 
 			<div> 
+<<<<<<< HEAD
 				<p> Cbs section  </p>
+=======
+				<p> Cbs section </p>
+>>>>>>> 335bba6... Update cbs.js
 				<p> 11:46 pm 08-10 </p>
 				
 			</div>
