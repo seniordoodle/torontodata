5515293e15e5b84abbe48450ffac19d219c13e93
Michael MacDonald <michael.macd@hotmail.com>
2020-10-08 23:44:38 -0400

    aa

--- src/orgs/home.js
+++ src/orgs/home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import kidneypic from '..\\Infograph\\InfoKidney.png';
 
 class Home extends Component { 
 	render() {
 		return ( 
 			<div> 
                 <p>  HOME section </p>
+				<img className='infoImg' alt='' src={kidneypic} ></img>
 			</div>
 		);
 	}
